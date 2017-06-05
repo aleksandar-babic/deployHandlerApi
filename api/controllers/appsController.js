bbb7caa33828148581659ef470ea37db7afd09a0
aleksandarbabic <alekski64@icloud.com>
2017-06-05 23:11:06 +0200

    Minor

--- api/controllers/appsController.js
+++ api/controllers/appsController.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 exports.getAppList = function(req, res) {
     App.find({}, function(err, app) {
         if (err)
-            res.status(500).send(err);
+            return res.status(500).send(err);
         res.status(200).json(app);
     });
 };
@@ -14,7 +14,7 @@ exports.addApp = function(req, res) {
     var app = new App(req.body);
     app.save(function(err, app) {
         if (err)
-            res.status(500).send(err);
+            return res.status(500).send(err);
         res.status(201).json(app);
     });
 };
@@ -22,9 +22,9 @@ exports.addApp = function(req, res) {
 exports.viewApp = function(req, res) {
     App.findById(req.params.appId, function(err, app) {
         if (err)
-            res.status(500).send(err);
+            return res.status(500).send(err);
         if (!app)
-            res.status(404).json({ message: 'Could not find app with that id.' });
+            return res.status(404).json({ message: 'Could not find app with that id.' });
         res.status(200).json(app);
     });
 };
@@ -45,7 +45,7 @@ exports.deleteApp = function(req, res) {
         _id: req.params.appId
     }, function(err, app) {
         if (err)
-            res.status(404).send(err);
+            return res.status(404).send(err);
         res.status(200).json({ message: 'App has been deleted.' });
     });
 };
